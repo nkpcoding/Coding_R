19469ec1fdda35b07add5ea3b6e5519c1717d8c7
nkpcoding <navakathrynp@mail.tau.ac.il>
2025-12-20 15:19:46 +0200

    Update Lesson 1 Code

--- Lesson 7/Lesson 1 Upload/r_assignment_1_week_1.R
+++ Lesson 7/Lesson 1 Upload/r_assignment_1_week_1.R
@@ -10,6 +10,11 @@
       participant_gender = c("male", "female", "nonbinary", "male", "female", "female")
       participant_age = c(17, 32, 26, 29, 33, 27) # age of participant (between 15-40)
       has_depression = c (0, 1, 1, 0, 0, 1) # whether the participant doesn't have depression (=0) or has depression (=1)
+      participant_num = c(1:6) # participant number
+      genders= c("male", "female", "nonbinary") # Creates a vector of gender
+      participant_gender = sample(genders,size = 6, replace = T,) # Creates a vector with participant genders
+      participant_age = c(sample(range(15, 40, size= 6, replace= T))) # age of participant (between 15-40)
+      has_depression = rbinom(6, 1, prob = 0.5) # whether the participant doesn't have depression (=0) or has depression (=1)
   
   # 2. Dataset Creation and Viewing
       
